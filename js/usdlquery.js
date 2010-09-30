6b106129912eb5ad55113a8bc81ebaec8bde4f38
Dawit <dawit.ayalew@tabyengineering.com>
2010-09-30 20:14:17 +0300

    Bigup on optimization the JQuery code dblclick editor end small custimization on css.

--- js/usdlquery.js
+++ js/usdlquery.js
@@ -1,15 +1,40 @@
 
-function display(usdlData,div,editable,callback) {
-  if(usdlData==null||div==null) {
+function display(usdlData,content,editable,callback) {
+  if(usdlData==null||content==null) {
     return;
   }
-  div.empty();
+  content.empty();
+	var guid = usdlData.find('guid').text();
+	var namespace = usdlData.find('namespace').text();
+	var name = usdlData.find('name').text();
 
-	var content = usdlData.find('guid').text();
+	content.append("<div id='content'> USDL Plugin  </div>");
+	content.append(
+			"<div class=\"ele\"> <div class=\"etit\">Guid:</div><div id='guid' class=\"eval\">" +guid+ "</div></div>"+
+			"<div class=b> NameSpace: <div id='namesp'>"+namespace+  "</div></div>"+
+			"<div class=c> Name: <div id='name'>"+name+"</div> </div>"
+			);
+			
 
-	div.append(content);
+	$("#guid").dblclick( function() {
+		var x	= $("#guid").text();
+		$("#guid").empty();
+		
+		$("#guid").append("<input id=\"text\" type=\"text\" value=\""+x+"\"/>");
+	});
+	$("body").click( function(evt) {
+		if($("#text").length>0) {
+			if(evt.target.id != "text") {
+				var y = $('#text').val();
+				$('#guid').empty();
+				$('#guid').append(y);
+			}
+		}
+	});
 }
 
-function render(div) {
-  return "<xml>yippi</xml>"
+function render(content) {
+  return "<xml></xml>";
 }
+
+
